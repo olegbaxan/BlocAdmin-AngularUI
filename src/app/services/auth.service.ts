ba8cb2a7957acdbbc4a08e97ca082474282c9383
olegbaxan <43432810+olegbaxan@users.noreply.github.com>
2021-12-14 20:09:53 +0200

    some more logging

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Router} from "@angular/router";
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
 
-const AUTH_API = 'http://localhost:8080/api/v1/auth/';
+// const AUTH_API = 'http://localhost:8080/api/v1/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -16,7 +16,7 @@ export class AuthService {
   private baseUrl = '/api/v1/auth/';
   loginUsername = '';
   constructor(private http: HttpClient,
-              private router: Router,) {
+              private router: Router) {
 
   }
   login(username: string, password: string): Observable<any> {
@@ -27,6 +27,7 @@ export class AuthService {
       username,
       password
     }, httpOptions);
+    console.log('HTTP', this.http);
   }
 
   register(username: any, email: any, password: any, description: any, name: any, surname: any, idnp: any, mobile: any): Observable<any> {
