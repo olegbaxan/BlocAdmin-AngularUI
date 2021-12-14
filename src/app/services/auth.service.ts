6b8f081993faff9738dfffc49fb38bf4c5bdf3fd
olegbaxan <43432810+olegbaxan@users.noreply.github.com>
2021-12-14 22:56:06 +0200

    update url

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Router} from '@angular/router';
+import {environment} from '../../environments/environment.prod';
 
 // const AUTH_API = 'http://localhost:8080/api/v1/auth/';
 
@@ -13,7 +14,7 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class AuthService {
-  private baseUrl = 'https://blocadmin-angularui.herokuapp.com/api/v1/auth/';
+  private baseUrl = environment.apiUrl + '/api/v1/auth/';
   loginUsername = '';
   constructor(private http: HttpClient,
               private router: Router) {
